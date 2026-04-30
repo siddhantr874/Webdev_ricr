c8ac9de5cdd1555a986877c1d8a2542f7029f005
siddhantr874 <siddhantr874@gmail.com>
2026-04-30 19:38:58 +0530

    shortend method

--- javascript/hello.js
+++ javascript/hello.js
@@ -30,12 +30,42 @@
 //   console.log("odd number");
 // }
 
-// let n = 20;
+// let n = 5;
 
-// if (n>18){
-//   console.log("eleigible to vote");
+// if (n>0){
+//   console.log("positive integer");
 // }
 // else{
-//   console.log("not eleigible to vote");
+//   console.log("negative integer");
 // }
+
+// let n = 93;
+
+// elseif (n>90){
+//   console.log("A+");
+// }
+// elseif (n>=75){
+//   console.log("B");
+// }
+// elseif (n>=50){
+//   console.log("C");
+// }
+// else{
+//   console.log("Fail");
+// }
+
+// let user = "abcde";
+// let pass = "12345";
+
+// user === "abcde" && pass==="12345"
+//   ?console.log("login success")
+//   :console.log("login failed") //shortend method
+
+let data;
+let name = "siddhant";
+
+data = name || 'N/A'
+
+console.log(data);
+
 
