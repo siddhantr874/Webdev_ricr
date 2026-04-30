c507726aff1aae5b754aca60a8ddc061927a8ca7
siddhantr874 <siddhantr874@gmail.com>
2026-04-30 18:54:59 +0530

    conditional statements

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,41 @@
-console.log("hello world")
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log("2 x " + i + " = " + (2 * i));
+//   i++;
+// }
+
+// let i = 1;
+
+// do {
+//   console.log("2 x " + i + " = " + (2 * i));
+//   i++;
+// } while (i <= 10);
+
+// let n = 5;
+
+// if (n>0){
+//   console.log("positive integer");
+// }
+// else{
+//   console.log("negative integer");
+// }
+
+// let n = 10;
+
+// if (n/2){
+//   console.log("even number");
+// }
+// else{
+//   console.log("odd number");
+// }
+
+// let n = 20;
+
+// if (n>18){
+//   console.log("eleigible to vote");
+// }
+// else{
+//   console.log("not eleigible to vote");
+// }
+
