18bfa5c2afddd56a045e6e81046f18554ef4db04
siddhantr874 <siddhantr874@gmail.com>
2026-05-01 18:57:31 +0530

    function and methods

---
+++ javascript/function.js
@@ -0,0 +1,25 @@
+// let a=20,b=30;
+
+// function sum(x,y){
+//     let z = x+y
+//     console.log(z);
+// }
+
+// sum(a,b);
+
+// let a=20,b=30;
+
+// function sum(x,y){
+//     let z = x+y
+//     return a+b;
+
+// }
+
+// let result = add(a,b);
+// console.log(result);
+
+let abc = function(){
+    console.log("hello world");
+};
+
+console.log(abc);
